a70518d8fd95243b6da09d8fd423e49f4079782a
giovankes <hello@giotje.dev>
2023-02-13 20:09:51 +0100

    formatter now works

--- src/lib.rs
+++ src/lib.rs
@@ -10,37 +10,34 @@ struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
-    }
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-    }
+  async fn ready(&self, _: Context, ready: Ready) {
+    info!("{} is connected!", ready.user.name);
+  }
 }
 
 #[shuttle_service::main]
 async fn serenity(
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+  #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_service::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
-        token
-    } else {
-        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
-    };
+  // Get the discord token set in `Secrets.toml`
+  let token = if let Some(token) =
+    secret_store.get("DISCORD_TOKEN")
+  {
+    token
+  } else {
+    return Err(
+      anyhow!("'DISCORD_TOKEN' was not found").into(),
+    );
+  };
 
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+  // Set gateway intents, which decides what events the bot will be notified about
+  let intents = GatewayIntents::GUILD_MESSAGES
+    | GatewayIntents::MESSAGE_CONTENT;
 
-    let client = Client::builder(&token, intents)
-        .event_handler(Bot)
-        .await
-        .expect("Err creating client");
+  let client = Client::builder(&token, intents)
+    .event_handler(Bot)
+    .await
+    .expect("Err creating client");
 
-    Ok(client)
+  Ok(client)
 }
