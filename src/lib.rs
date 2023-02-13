a1993dbdca71c80a9f8d3aad2a3fb693eed12389
giovankes <hello@giotje.dev>
2023-02-13 20:26:19 +0100

    bot now compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use anyhow::anyhow;
 use serenity::async_trait;
-use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::interaction::Interaction;
+use serenity::model::prelude::interaction::InteractionResponseType;
+use serenity::model::prelude::GuildId;
 use serenity::prelude::*;
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
@@ -10,34 +12,60 @@ struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
-  async fn ready(&self, _: Context, ready: Ready) {
-    info!("{} is connected!", ready.user.name);
-  }
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+        let guild_id = GuildId(1052300006633259009);
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands.create_application_command(|command| {
+                command.name("hello").description("say hello")
+            })
+        })
+        .await
+        .unwrap();
+        info!("{:#?}", commands);
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let response_content = match command.data.name.as_str() {
+                "hello" => "hello".to_owned(),
+
+                command => unreachable!("Unknown command: {}", command),
+            };
+
+            let create_interaction_response =
+                command.create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(response_content))
+                });
+
+            if let Err(why) = create_interaction_response.await {
+                eprintln!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
 }
 
 #[shuttle_service::main]
 async fn serenity(
-  #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_service::ShuttleSerenity {
-  // Get the discord token set in `Secrets.toml`
-  let token = if let Some(token) =
-    secret_store.get("DISCORD_TOKEN")
-  {
-    token
-  } else {
-    return Err(
-      anyhow!("'DISCORD_TOKEN' was not found").into(),
-    );
-  };
+    // Get the discord token set in `Secrets.toml`
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
+        token
+    } else {
+        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
+    };
 
-  // Set gateway intents, which decides what events the bot will be notified about
-  let intents = GatewayIntents::GUILD_MESSAGES
-    | GatewayIntents::MESSAGE_CONTENT;
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
-  let client = Client::builder(&token, intents)
-    .event_handler(Bot)
-    .await
-    .expect("Err creating client");
+    let client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .await
+        .expect("Err creating client");
 
-  Ok(client)
+    Ok(client)
 }
