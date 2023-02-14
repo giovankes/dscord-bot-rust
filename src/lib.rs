fe71495b9262e95e94ae24d5bb8f1dd75a7fc0a2
giovankes <hello@giotje.dev>
2023-02-14 20:24:19 +0100

    finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
 use anyhow::anyhow;
+use reqwest::Client;
 use serenity::async_trait;
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::command::CommandOptionType;
 use serenity::model::prelude::interaction::Interaction;
 use serenity::model::prelude::interaction::InteractionResponseType;
 use serenity::model::prelude::GuildId;
 use serenity::prelude::*;
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
+mod weather;
 
-struct Bot;
+struct Bot {
+    client: Client,
+    discord_guild_id: GuildId,
+}
 
 #[async_trait]
 impl EventHandler for Bot {
@@ -17,9 +23,22 @@ impl EventHandler for Bot {
         let guild_id = GuildId(1052300006633259009);
 
         let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
-            commands.create_application_command(|command| {
-                command.name("hello").description("say hello")
-            })
+            commands
+                .create_application_command(|command| {
+                    command.name("hello").description("say hello")
+                })
+                .create_application_command(|command| {
+                    command
+                        .name("weather")
+                        .description("Display the weather")
+                        .create_option(|option| {
+                            option
+                                .name("place")
+                                .description("City to lookup forecast")
+                                .kind(CommandOptionType::String)
+                                .required(true)
+                        })
+                })
         })
         .await
         .unwrap();
@@ -30,6 +49,27 @@ impl EventHandler for Bot {
         if let Interaction::ApplicationCommand(command) = interaction {
             let response_content = match command.data.name.as_str() {
                 "hello" => "hello".to_owned(),
+                "weather" => {
+                    let argument = command
+                        .data
+                        .options
+                        .iter()
+                        .find(|opt| opt.name == "place")
+                        .cloned();
+
+                    let value = argument.unwrap().value.unwrap();
+                    let place = value.as_str().unwrap();
+                    let result = weather::get_forecast(place).await;
+
+                    match result {
+                        Ok((location, forecast)) => {
+                            format!("Forecast: {} in {}", forecast.headline.overview, location)
+                        }
+                        Err(err) => {
+                            format!("Err: {}", err)
+                        }
+                    }
+                }
 
                 command => unreachable!("Unknown command: {}", command),
             };
@@ -59,11 +99,27 @@ async fn serenity(
         return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
     };
 
+    let weather_api_key = if let Some(weather_api_key) = secret_store.get("WEATHER_API_KEY") {
+        weather_api_key
+    } else {
+        return Err(anyhow!("weather api key not found").into());
+    };
+
+    let discord_guild_id = if let Some(discord_guild_id) = secret_store.get("DISCORD_GUILD_ID") {
+        discord_guild_id
+    } else {
+        return Err(anyhow!("discord guild id not found").into());
+    };
+
     // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let client = Client::builder(&token, intents)
-        .event_handler(Bot)
+        .event_handler(Bot {
+            weather_api_key,
+            client: reqwest::Client::new(),
+            discord_guild_id: GuildId(discord_guild_id.parse().unwrap()),
+        })
         .await
         .expect("Err creating client");
 
